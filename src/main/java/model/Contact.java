5d7df63f996b452f4991fc0adaa7b3a7ce4b63e4
Marek Maliszewski <mkmaliszewski@gmail.com>
2017-05-19 15:42:16 +0200

    small changes to contact class

--- src/main/java/model/Contact.java
+++ src/main/java/model/Contact.java
@@ -1,12 +1,16 @@
 package model;
 
-public class Contact {
+import java.io.Serializable;
+
+public class Contact implements Serializable{
     private int id;
     private String name;
     private String lastName;
     private String mail;
     private String number;
     private String bornDate;
+    
+    public Contact() {}
     
     public int getId(){
         return id;
